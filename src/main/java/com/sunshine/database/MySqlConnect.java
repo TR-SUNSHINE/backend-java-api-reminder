b340de763843dc56ce86f4274749b9a07cee7019
Chenerywoman <rachelmchenery@hotmail.com>
2021-03-17 16:05:56 +0000

    separating out database connection & close to separate class

---
+++ src/main/java/com/sunshine/database/MySqlConnect.java
@@ -0,0 +1,64 @@
+package com.sunshine.database;
+
+import com.sunshine.GetReminderHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.*;
+
+public class MySqlConnect {
+
+    private static final Logger LOG = LogManager.getLogger(GetReminderHandler.class);
+
+    private Connection connection = null;
+
+    public Connection connect() {
+        if (connection == null) {
+
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+
+
+                LOG.debug(String.format("Connecting to DB on %s", System.getenv("DB_HOST")));
+
+                connection = DriverManager.getConnection(String.format("jdbc:mysql://%s/%s?user=%s" +
+                                "&password=%s&useSSL=false",
+                        System.getenv("DB_HOST"),
+                        System.getenv("DB_NAME"),
+                        System.getenv("DB_USER"),
+                        System.getenv("DB_PASSWORD")));
+
+            } catch (ClassNotFoundException exception) {
+
+                LOG.error(String.format("ClassNotFoundException: %s", exception.getMessage()), exception);
+
+            } catch (SQLException exception) {
+
+                LOG.error(String.format("SQL exception: %s", exception.getMessage()), exception);
+            }
+        }
+
+        return connection;
+    }
+
+    public void closeConnection() {
+
+        try {
+
+//            if (resultSet != null){
+//                resultSet.close();
+//            }
+//
+//            if (preparedStatement != null) {
+//                preparedStatement.close();
+//            }
+
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException exception) {
+            LOG.error("Unable to close connection to MySQL - {}", exception.getMessage());
+        }
+    }
+
+}
