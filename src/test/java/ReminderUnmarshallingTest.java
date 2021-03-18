17688ffd90c54898e8ac216a0d0673c4b6c98302
Chenerywoman <rachelmchenery@hotmail.com>
2021-03-18 12:54:29 +0000

    adding unmarshalling test

--- src/test/java/ReminderUnmarshallingTest.java
+++ src/test/java/ReminderUnmarshallingTest.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class ReminderUnmarshallingTest {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.sunshine.model.Reminder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class ReminderUnmarshallingTest {
+
+    @Test
+    void testUnmarshallReminder() throws IOException {
+        String requestBody = "{" +
+                "\"reminderTime\": \"2020-09-30T07:27:39Z\"" +
+                "}";
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        Reminder reminder = objectMapper.readValue(requestBody, Reminder.class);
+        Assertions.assertNotNull(reminder.getReminderTime());
+        System.out.println(reminder.getReminderTime().toString());
+    }
+
 }
