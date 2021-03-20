9c584b733ab8411cbdbfa85110fe018c74b6e411
Chenerywoman <rachelmchenery@hotmail.com>
2021-03-20 14:17:12 +0000

    updating to call reminderService.changeReminder method instead fof mySqlConnect.updateReminder method

--- src/main/java/com/sunshine/handler/UpdateReminderHandler.java
+++ src/main/java/com/sunshine/handler/UpdateReminderHandler.java
@@ -9,6 +9,7 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.sunshine.service.ReminderService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -25,7 +26,7 @@ public class UpdateReminderHandler implements RequestHandler<APIGatewayProxyRequ
     private static final Logger LOG = LogManager.getLogger(SaveReminderHandler.class);
 
     MySqlConnect mySqlConnect = new MySqlConnect();
-    private ResultSet resultSet = null;
+    private final ReminderService reminderService = new ReminderService();
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request,
@@ -35,43 +36,38 @@ public class UpdateReminderHandler implements RequestHandler<APIGatewayProxyRequ
 
         String UserId = request.getPathParameters().get("userId");
         String ReminderId = request.getPathParameters().get("reminderId");
-        LOG.info(String.format("ReminderId %s",
-                ReminderId));
         String requestBody = request.getBody();
 
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
 
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
-//        response.setStatusCode(200);
+        response.setStatusCode(200);
         Map<String, String> headers = new HashMap<>();
         headers.put("Access-Control-Allow-Origin", "*");
         response.setHeaders(headers);
 
+        Reminder reminder = null;
+
         try {
 
-            Reminder reminder = objectMapper.readValue(requestBody, Reminder.class);
+            reminder = objectMapper.readValue(requestBody, Reminder.class);
             reminder.setUserId(UserId);
             reminder.setReminderId(ReminderId);
 
-            if  (mySqlConnect.updateReminder(reminder) == 1) {
-                LOG.debug("updated");
-                response.setStatusCode(200);
-            } else {
-                LOG.debug("update failed");
-                response.setStatusCode(500);
-            }
-
-            // send reminderId to frontend
-
         } catch (IOException exception) {
-            LOG.error(String.format("Unable to unmarshall JSON for adding a reminder %s",
+            LOG.error(String.format("Unable to unmarshall JSON for updating a reminder %s",
                     exception.getMessage()));
             response.setStatusCode(500);
         }
 
-        finally {
-            mySqlConnect.closeConnection();
+        if (reminderService.changeReminder(reminder) == 1) {
+
+            LOG.info("updated");
+
+        } else {
+            LOG.info("update failed");
+            response.setStatusCode(500);
         }
 
         return response;
