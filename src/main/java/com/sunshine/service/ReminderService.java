2c12f863e56a56320d53ee48e41f68e08cd7f2a1
Chenerywoman <rachelmchenery@hotmail.com>
2021-03-20 10:41:21 +0000

    adding saveReminder to ReminderService

--- src/main/java/com/sunshine/service/ReminderService.java
+++ src/main/java/com/sunshine/service/ReminderService.java
@@ -1,2 +1,72 @@
-package com.sunshine.service;public class ResponseService {
+package com.sunshine.service;
+
+import com.sunshine.model.Reminder;
+import com.sunshine.database.MySqlConnect;
+
+import java.sql.*;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import org.apache.logging.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ReminderService {
+
+    private MySqlConnect mySqlConnect = new MySqlConnect();
+
+    private static final Logger LOG = LogManager.getLogger(ReminderService.class);
+
+    public APIGatewayProxyResponseEvent saveReminder(Reminder reminder) {
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Access-Control-Allow-Origin", "*");
+        response.setHeaders(headers);
+
+        if (mySqlConnect.createReminder(reminder) == 1) {
+
+            LOG.debug("value of createReminder: {}",mySqlConnect.createReminder(reminder) );
+            response.setStatusCode(201);
+
+        } else {
+            response.setStatusCode(500);
+        }
+
+        mySqlConnect.closeConnection();
+
+        return response;
+
+    }
+
+
+    public APIGatewayProxyResponseEvent getReminder(Reminder reminder) {
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        response.setStatusCode(200);
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Access-Control-Allow-Origin", "*");
+        response.setHeaders(headers);
+
+        try {
+
+            mySqlConnect.createReminder(reminder);
+
+
+        } catch (Exception exception) {
+
+
+        } finally {
+            mySqlConnect.closeConnection();
+        }
+
+        return response;
+
+    }
+
+
 }
