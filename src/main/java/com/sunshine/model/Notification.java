a9f22a01cf3d6ce96522f75a56a946676e7c7416
Chenerywoman <rachelmchenery@hotmail.com>
2021-03-26 13:15:02 +0000

    adding code to Notification class

--- src/main/java/com/sunshine/model/Notification.java
+++ src/main/java/com/sunshine/model/Notification.java
@@ -1,4 +1,41 @@
 package com.sunshine.model;
 
+import java.time.LocalDateTime;
+
 public class Notification {
+
+    private String reminderId;
+    private LocalDateTime reminderTime;
+    private String userId;
+    private String email;
+    private String userName;
+
+    public Notification() {}
+
+    public Notification(String reminderId, LocalDateTime reminderTime, String userId,
+                        String email, String userName){
+        this.reminderId = reminderId;
+        this.reminderTime = reminderTime;
+        this.userId = userId;
+        this.email = email;
+        this.userName = userName;
+    }
+
+    public String getReminderId(){ return this.reminderId;}
+
+    public void setReminderId(String reminderId){this.reminderId = reminderId;}
+
+    public LocalDateTime getReminderTime(){ return this.reminderTime;}
+
+    public void setReminderTime(LocalDateTime reminderTime){ this.reminderTime =
+            reminderTime; }
+
+    public String getEmail(){ return this.email;}
+
+    public void setEmail(String email){this.email = email;}
+
+    public String getUserName(){return this.userName;}
+
+    public void setUserName(String userName){this.userId = userName;}
 }
+
